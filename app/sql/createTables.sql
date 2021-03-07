3ed2302ba9b4e38d3247b8bc390f936d823cf85f
Allen <allenjhuang92@gmail.com>
2021-03-06 18:03:47 -0600

    updated map

--- app/sql/createTables.sql
+++ app/sql/createTables.sql
@@ -6,7 +6,7 @@ CREATE TABLE Listings (
     listing_id SERIAL UNIQUE NOT NULL PRIMARY KEY,
     owner_name VARCHAR(30),
     owner_phone_num VARCHAR(30),
-    price NUMERIC(16, 2) NOT NULL,
+    price NUMERIC(16, 2),
     latitude NUMERIC(16) NOT NULL,
     longitude NUMERIC(16) NOT NULL,
     sell_date DATE
